3c9f756e51fe8acf9bd203dc262f3e6537c1705e
LeeJay0126 <topaz9889@gmail.com>
2022-10-23 19:28:07 -0700

    Home hero section done

--- langaratribe/src/Pages/Home/Home.js
+++ langaratribe/src/Pages/Home/Home.js
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import Nav from "../../Components/Nav";
+import Hero from "./HomeComponents/Hero";
 
 const Home = () => {
     const [heroState, setState] = useState("default");
+
     return (
         <div>
             <Nav/>
+            <Hero setState={setState}/>
         </div>
     );
 };
