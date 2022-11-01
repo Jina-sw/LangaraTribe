daa7652a18cef922086f9bac9686ebd49e8984b3
LeeJay0126 <topaz9889@gmail.com>
2022-10-31 18:19:47 -0700

    login page done, nav implemented

--- src/Pages/LogIn/LogIn.js
+++ src/Pages/LogIn/LogIn.js
@@ -1,12 +1,48 @@
 import React, { useContext } from "react";
-import { LoginContext } from "../../App";
+import { LoginContext } from "../../Components/LoginContext";
+import { AiOutlineUserAdd } from "react-icons/ai";
+import { RiLockPasswordLine } from "react-icons/ri";
+import LoginNav from "./LoginNav";
+import "./Login.css";
 
 export default function LogIn() {
-    const [isLoggedIn, setLoggedIn] = useContext(Context);
+    const { isLoggedIn, setLoggedIn } = useContext(LoginContext);
 
     return (
         <div>
-            <button onClick={()=> setLoggedIn(!isLoggedIn)}>Log In</button>
+            <LoginNav />
+            <form className="loginForm">
+                <h1 className="loginHeader">
+                    Login
+                </h1>
+                <section className="inputFields">
+                    <p className="inputFieldHeader">
+                        Username
+                    </p>
+                    <div className="inputPlaceHolder">
+                        <AiOutlineUserAdd className="inputIcon" />
+                        <input type="text" placeholder="Type your username" />
+                    </div>
+                </section>
+                <section className="inputFields">
+                    <p className="inputFieldHeader">
+                        Password
+                    </p>
+                    <div className="inputPlaceHolder second">
+                        <RiLockPasswordLine className="inputIcon" />
+                        <input type="text" placeholder="Type your password" />
+                    </div>
+                </section>
+                <div className="findPassword">
+                    <p>Forgot password?</p>
+                </div>
+                <section className="loginButton">
+                    <button onClick={() => setLoggedIn(!isLoggedIn)}>LOGIN</button>
+                </section>
+                <section className="loginPageSignUp">
+                    <p>Or Sign Up!</p>
+                </section>
+            </form>
         </div>
     );
 
